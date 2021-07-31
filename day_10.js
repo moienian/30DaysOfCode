d7d19bbe4ebd7261338590f6f44ec5c1d403926d
Mohammad Moien <mnmoienian@gmail.com>
2021-07-31 17:17:47 +0430

    add day_10 solution

---
+++ day_10.js
@@ -0,0 +1,34 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+function main() {
+  const n = parseInt(readLine().trim(), 10);
+
+  console.log(
+    Math.max(
+      ...n
+        .toString(2)
+        .split(/0+/)
+        .map((e) => e.length)
+    )
+  );
+}
