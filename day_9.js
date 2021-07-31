fcea24823e9475e1499bfc33bb2359fb2cfa5c02
Mohammad Moien <mnmoienian@gmail.com>
2021-07-31 16:04:31 +0430

    add day_9 solution

---
+++ day_9.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+function factorial(n) {
+  if (n === 1) {
+    return n;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const n = parseInt(readLine().trim(), 10);
+
+  const result = factorial(n);
+
+  ws.write(result + "\n");
+
+  ws.end();
+}
