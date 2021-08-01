b5837da9df80b2ce12c99623fd66139d7675d0c3
Mohammad Moien <mnmoienian@gmail.com>
2021-08-01 20:06:20 +0430

    add day_11 solution

---
+++ day_11.js
@@ -0,0 +1,58 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+function main() {
+  let arr = Array(6);
+
+  for (let i = 0; i < 6; i++) {
+    arr[i] = readLine()
+      .replace(/\s+$/g, "")
+      .split(" ")
+      .map((arrTemp) => parseInt(arrTemp, 10));
+  }
+
+  var sumArr = [];
+
+  var h = 0;
+  for (var i = 0; i < 4; i++) {
+    for (var j = 0; j < 4; j++) {
+      sumArr[h] =
+        arr[i][j] +
+        arr[i][j + 1] +
+        arr[i][j + 2] +
+        arr[i + 1][j + 1] +
+        arr[i + 2][j] +
+        arr[i + 2][j + 1] +
+        arr[i + 2][j + 2];
+      h++;
+    }
+  }
+
+  sumArr.sort();
+
+  var max = -99999;
+  for (var x = 0; x < 16; x++) {
+    if (sumArr[x] > max) max = sumArr[x];
+  }
+
+  console.log(max);
+}
